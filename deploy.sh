48b71c65dab2bc52f19a0ecd14b308f3be2142ac
Tamás Soós <tsoos99@gmail.com>
2025-05-25 21:40:18 +0200

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+set -e
+
+source .env
+
+ssh $SDK_SSH_HOST /bin/bash << ENDSSH 
+cd $REMOTE_PROJECT_DIR
+source $SDK_ENV_SETUP_SCRIPT
+git pull
+cargo build --target $TARGET_TRIPLE
+ENDSSH
+
+scp $SDK_SSH_HOST:$REMOTE_PROJECT_DIR/target/$TARGET_TRIPLE/debug/zeus-controller $TARGET_SSH_HOST:$TARGET_DEPLOYMENT_PATH
