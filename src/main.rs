93a690133569eea41d95a7796e0050db7b708d4f
Tamás Soós <tsoos99@gmail.com>
2025-05-25 21:40:30 +0200

    Switch to test main script

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,5 @@
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    use tokio_serial::SerialStream;
-
-    use tokio_modbus::prelude::*;
-
-    let tty_path = "/dev/ttyS1";
-    let slave = Slave(0x01);
-
-    let builder = tokio_serial::new(tty_path, 9600);
-    let port = SerialStream::open(&builder).unwrap();
-
-    let mut ctx = rtu::attach_slave(port, slave);
-    println!("Reading a sensor value");
-    let rsp = ctx.read_holding_registers(0x082B, 2).await??;
-    println!("Sensor value is: {rsp:?}");
-
-    println!("Disconnecting");
-    ctx.disconnect().await?;
-
+    println!("Hi!");
     Ok(())
 }
