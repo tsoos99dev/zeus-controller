e0dc4f412b58d875fa1d9b5af9030b19e4d11635
Tamás Soós <tsoos99@gmail.com>
2025-05-28 23:53:12 +0200

    Add profile option to deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,19 @@ set -e
 
 source .env.deploy
 
+CARGO_PROFILE_ARG=""
+
+if [ $PROFILE = "release" ]; then 
+    CARGO_PROFILE_ARG="--release";
+fi
+
+echo "Building app for the target..."
 ssh $SDK_SSH_HOST /bin/bash << ENDSSH 
 cd $REMOTE_PROJECT_DIR
 source $SDK_ENV_SETUP_SCRIPT
 git pull
-cargo build --target $TARGET_TRIPLE
+cargo build $CARGO_PROFILE_ARG --target $TARGET_TRIPLE
 ENDSSH
 
-scp $SDK_SSH_HOST:$REMOTE_PROJECT_DIR/target/$TARGET_TRIPLE/debug/zeus-controller $TARGET_SSH_HOST:$TARGET_DEPLOYMENT_PATH
+echo "Copying the binary..."
+scp $SDK_SSH_HOST:$REMOTE_PROJECT_DIR/target/$TARGET_TRIPLE/$PROFILE/zeus-controller $TARGET_SSH_HOST:$TARGET_DEPLOYMENT_PATH
